efb4b426025cae0c1835b3f8012fe651614440a5
Jose Esquinca <esquinca_jose@hotmail.com>
2022-05-14 13:11:52 -0500

    solving issues

---
+++ src/main/java/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,14 @@
+package guru.sfg.beer.inventory.service.web.mappers;
+
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.brewery.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
